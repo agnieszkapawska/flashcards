fc58be6e2261154f02bceeb36dff4fcd48031681
agnieszkapawska <54909606+agnieszkapawska@users.noreply.github.com>
2019-09-07 12:36:44 +0200

    Update src/main/java/com/agnieszkapawska/flashcards/domain/dtos/FlashcardResponseDto.java
    Co-Authored-By: dourado <pawski.krzysztof@gmail.com>

--- src/main/java/com/agnieszkapawska/flashcards/domain/dtos/FlashcardResponseDto.java
+++ src/main/java/com/agnieszkapawska/flashcards/domain/dtos/FlashcardResponseDto.java
@@ -7,6 +7,6 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class FlashcardResponseDto {
+public class FlashcardSaveResponseDto {
     private Long id;
 }
